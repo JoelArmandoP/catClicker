a6cd11f2a5b654bdf46ba7f1d3d1bf98b71e619a
Joel Armando <joelarma@gmail.com>
2015-11-02 22:17:47 +0000

    Add cat prototype and functions for two cats

--- js/main.js
+++ js/main.js
@@ -1,10 +1,40 @@
 'use strict';
 
-var clicks = 0;
-
-document.getElementById('cat_img').addEventListener('click', addClick);
-
-function addClick() {
-    clicks = clicks + 1;
-    document.getElementById('click').innerHTML = clicks;
+function cat (name, image) {
+    this.name = name;
+    this.image = image;
+    this.clicks = 0;
 }
+
+var cats = [new cat('Alfonsina', 'images/cat1.jpg'),
+			new cat('Margarita', 'images/cat2.jpg')];
+
+var HTMLcat =
+  '<div class="col-md-6">'+
+    '<div class="cat-name">%name%</div>'+
+    '<div class="img">%image%</div>'+
+    '<div class="clicks">%clicks%</div></div>';
+
+cats.display = function() {
+	cats.forEach(
+		function(c, catNumber) {
+			var imageId = 'cat-image-' + catNumber;
+			var clicksId = 'cat-clicks-' + catNumber
+			$('#cats').append(
+				HTMLcat.
+				replace(/%name%/g, c.name).
+				replace(/%image%/g, '<img id="' + imageId + '" src="' + c.image + '">').
+				replace(/%clicks%/g, '<span id="' + clicksId + '">' + c.clicks + "</span>")
+			);
+			document.getElementById(imageId).addEventListener('click', addClickFctn(c, document.getElementById(clicksId)));
+		});
+};
+
+function addClickFctn(cat, clickDisplay) {
+	return function() {
+    	cat.clicks++;
+	    clickDisplay.innerHTML = cat.clicks;
+	}
+}
+
+cats.display();
