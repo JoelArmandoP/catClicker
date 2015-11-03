b2128e3054060f64d74d950bd0a7c67ec9e57a1d
Joel Armando <joelarma@gmail.com>
2015-11-03 21:04:22 +0000

    Add list of cats and modify display function to meet the new requirements

--- js/main.js
+++ js/main.js
@@ -7,28 +7,47 @@ function cat (name, image) {
 }
 
 var cats = [new cat('Alfonsina', 'images/cat1.jpg'),
-			new cat('Margarita', 'images/cat2.jpg')];
+			new cat('Margarita', 'images/cat2.jpg'),
+			new cat('Roberto', 'images/cat3.jpg'),
+			new cat('Rudolf', 'images/cat4.jpg'),
+			new cat('Gatitow', 'images/cat5.jpg')
+			];
 
 var HTMLcat =
   '<div class="col-md-6">'+
     '<div class="cat-name">%name%</div>'+
     '<div class="img">%image%</div>'+
     '<div class="clicks">%clicks%</div></div>';
+var HTMLcatList =
+    '<li id="%cat-id%" class="cat-name">%name%</li>';
 
-cats.display = function() {
+cats.displayList = function() {
 	cats.forEach(
 		function(c, catNumber) {
-			var imageId = 'cat-image-' + catNumber;
-			var clicksId = 'cat-clicks-' + catNumber
+			var catId = 'cat-'+ catNumber;
 			$('#cats').append(
-				HTMLcat.
-				replace(/%name%/g, c.name).
-				replace(/%image%/g, '<img id="' + imageId + '" src="' + c.image + '">').
-				replace(/%clicks%/g, '<span id="' + clicksId + '">' + c.clicks + "</span>")
+				HTMLcatList.
+				replace(/%cat-id%/g, catId).
+				replace(/%name%/g, c.name)
 			);
-			document.getElementById(imageId).addEventListener('click', addClickFctn(c, document.getElementById(clicksId)));
+			document.getElementById(catId).addEventListener('click', displayCatFctn(c));
+
 		});
 };
+
+
+function displayCatFctn(cat) {
+	return function() {
+		$('#catSelected').html(
+		HTMLcat.
+		replace(/%name%/g, cat.name).
+		replace(/%image%/g, '<img id="cat-image" src="' + cat.image + '">').
+		replace(/%clicks%/g, '<span id="cat-clicks">' + cat.clicks + "</span>")
+		);
+		document.getElementById('cat-image').addEventListener('click', addClickFctn(cat, document.getElementById('cat-clicks')));
+	}
+}
+
 
 function addClickFctn(cat, clickDisplay) {
 	return function() {
@@ -37,4 +56,4 @@ function addClickFctn(cat, clickDisplay) {
 	}
 }
 
-cats.display();
+cats.displayList();
