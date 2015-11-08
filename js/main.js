f13aaf034fd0801a09b0383d4704385d11bb4c1b
Joel Armando <joelarma@gmail.com>
2015-11-08 14:34:07 +0000

    Refactor main.js to apply Model-View- Octopus

--- js/main.js
+++ js/main.js
@@ -1,57 +1,104 @@
 'use strict';
 
-function cat (name, image) {
+function cat(name, image) {
     this.name = name;
     this.image = image;
     this.clicks = 0;
-}
+};
 
-var cats = [new cat('Alfonsina', 'images/cat1.jpg'),
-			new cat('Margarita', 'images/cat2.jpg'),
-			new cat('Roberto', 'images/cat3.jpg'),
-			new cat('Rudolf', 'images/cat4.jpg'),
-			new cat('Gatitow', 'images/cat5.jpg')
-			];
-
-var HTMLcat =
-  '<div class="col-md-6">'+
-    '<div class="cat-name">%name%</div>'+
-    '<div class="img">%image%</div>'+
-    '<div class="clicks">%clicks%</div></div>';
-var HTMLcatList =
-    '<li id="%cat-id%" class="cat-name">%name%</li>';
-
-cats.displayList = function() {
-	cats.forEach(
-		function(c, catNumber) {
-			var catId = 'cat-'+ catNumber;
-			$('#cats').append(
-				HTMLcatList.
-				replace(/%cat-id%/g, catId).
-				replace(/%name%/g, c.name)
-			);
-			document.getElementById(catId).addEventListener('click', displayCatFctn(c));
-
-		});
+var model = {
+	init: function() {
+		model.cats = [];
+	},
+	addCat: function(name, image) {
+		model.cats.push(new cat(name, image));
+	},
+	getCats: function() {
+		return model.cats;
+	},
+	getCat: function(i) {
+		return model.cats[i];
+	},
+	getNumberOfCats: function() {
+		return model.cats.length;
+	}
 };
 
 
-function displayCatFctn(cat) {
-	return function() {
-		$('#cat-name').text(cat.name);
-		$('#cat-image').attr('src', cat.image);
-		$('#cat-image').off('click');
-		$('#cat-image').click(addClickFctn(cat, $('#cat-clicks')));
-		$('#cat-clicks').text(cat.clicks);
+var catView = {
+	init: function() {
+		catView.catName = $('#cat-name');
+		catView.catImage = $('#cat-image');
+		catView.catClicks = $('#cat-clicks');
+	},
+	render: function(cat) {
+		catView.catName.text(cat.name);
+		catView.catImage.attr('src', cat.image);
+		catView.catClicks.text(cat.clicks);
+	},
+	/* callback: f()
+	*/
+	onLike: function(callback) {
+		catView.catImage.off('click');
+		catView.catImage.click(callback);
 	}
-}
+};
 
-
-function addClickFctn(cat, clickDisplay) {
-	return function() {
-    	cat.clicks++;
-	    clickDisplay.text(cat.clicks);
+var catListView = {
+	init: function() {
+		catListView.catList = $('#cat-list');
+		catListView.onSelectCallback = null;
+	},
+	render: function(cats) {
+		var items = '';
+		cats.forEach(
+			function(cat, catNo) {
+				items += '<li id="cat-'+ catNo + '">' + cat.name + '</li>';
+			});
+		catListView.catList.html(items);
+		cats.forEach(
+			function(cat, catNo) {
+				$('#cat-'+ catNo).click(catListView.clickHandlerFctn(cat));
+			});
+	},
+	clickHandlerFctn: function(cat) {
+		return function() {
+			if (catListView.onSelectCallback) {
+				catListView.onSelectCallback(cat);
+			}
+		}
+	},
+	/* callback: f(cat)
+	*/
+	onSelect: function(callback) {
+		catListView.onSelectCallback = callback;
 	}
-}
+};
 
-cats.displayList();
+var octopus = {
+	init: function() {
+		model.init();
+		model.addCat('Alfonsina', 'images/cat1.jpg');
+		model.addCat('Margarita', 'images/cat2.jpg');
+		model.addCat('Roberto', 'images/cat3.jpg');
+		model.addCat('Rudolf', 'images/cat4.jpg');
+		model.addCat('Gatitow', 'images/cat5.jpg');
+		catListView.init();
+		catListView.onSelect(octopus.selectCat);
+		catListView.render(model.getCats());
+		catView.init();
+		catView.onLike(octopus.likeCat);
+		octopus.selectedCat = model.getCat(0);
+		catView.render(octopus.selectedCat);
+	},
+	likeCat: function () {
+		octopus.selectedCat.clicks++;
+		catView.render(octopus.selectedCat);
+	},
+	selectCat: function(cat) {
+		octopus.selectedCat = cat;
+		catView.render(cat);
+	}
+};
+
+octopus.init();
